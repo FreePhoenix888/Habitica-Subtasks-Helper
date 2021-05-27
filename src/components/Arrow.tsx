17364fb44dc13e714aa5b5516ae12e5a17a75f0e
FreePhoenix <freephoenix888@gmail.com>
2021-05-27 22:32:42 +0600

    Arrow, PopUp components are added

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {
+   className?: string;
+   direction: string;
+}
+
+export function Arrow(props: Props){
+   
+}
---
+++ src/components/PopUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {
+	className?: string;
+	children: string | Element | Element[] | JSX.Element | JSX.Element[];
+};
+export function PopUp(props: Props) {
+	const { className, children } = props;
+	return <div className={`popup ${className}`}>{children}</div>;
+}
