15bac1d8cd576966be67d985712655ab025d1f44
FreePhoenix <freephoenix888@gmail.com>
2021-05-27 21:51:04 +0600

    Paragraph inside the InfoButton component is removed

---
+++ src/components/InfoButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type Props = {
+	className?: string;
+	paragraphClassName?: string;
+	hidden?: boolean;
+	onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+   onMouseOver?: (event: React.MouseEvent<HTMLElement>) => void;
+   onFocus?: (event: React.FocusEvent<HTMLElement>) => void;
+   onKeyDown?: (event: React.KeyboardEvent<HTMLElement>) => void;
+};
+export function InfoButton(props: Props) {
+	const {
+		className = '',
+		paragraphClassName = '',
+		hidden = false,
+		onClick = () => {},
+      onMouseOver = () => {},
+      onFocus = () => {},
+      onKeyDown = () => {}
+	} = props;
+	let value;
+	return (
+		<button
+			className={`div info ${className}`}
+         onClick={onClick}
+			onMouseOver={onMouseOver}
+         onFocus={onFocus}
+         onKeyDown={onKeyDown}
+			hidden={hidden}
+			type="button"
+		>
+			?
+		</button>
+	);
+}
