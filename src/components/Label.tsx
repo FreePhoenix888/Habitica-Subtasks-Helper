d87c92cfeeafd8be83f783ce98018a41388fd0ae
FreePhoenix <freephoenix888@gmail.com>
2021-06-01 00:52:06 +0600

    htmlFor and children props are not required

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 type Props = {
 	className?: string;
-	htmlFor: string | undefined;
-	children: string | Element | Element[] | JSX.Element | JSX.Element[];
+	htmlFor?: string | undefined;
+	children?: string | Element | Element[] | JSX.Element | JSX.Element[];
 };
 
 export function Label(props: Props): JSX.Element {
