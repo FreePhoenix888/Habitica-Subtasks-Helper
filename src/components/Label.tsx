3b972477675afe041300492ec9f575d317c2293e
FreePhoenix <freephoenix888@gmail.com>
2021-06-01 14:34:37 +0600

    Content got event handlers

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,15 +2,41 @@ import React from 'react';
 
 type Props = {
 	className?: string;
+	contentClassName?: string;
 	htmlFor?: string | undefined;
 	children?: string | Element | Element[] | JSX.Element | JSX.Element[];
+	onClickHandler?: (event: React.MouseEvent<HTMLElement>) => void;
+	onKeyDownHandler?: (event: React.KeyboardEvent<HTMLElement>) => void;
 };
 
 export function Label(props: Props): JSX.Element {
-	const { htmlFor, children, className='' } = props;
+	const { htmlFor, children, className = '', contentClassName = '' } = props;
+
+	function handleClick(event: React.MouseEvent<HTMLElement>) {
+		const { onClickHandler } = props;
+		if (onClickHandler) {
+			onClickHandler(event);
+		}
+	}
+
+	function handleKeyDown(event: React.KeyboardEvent<HTMLElement>) {
+		const { onKeyDownHandler } = props;
+		if (onKeyDownHandler) {
+			onKeyDownHandler(event);
+		}
+	}
+
 	return (
 		<label htmlFor={htmlFor} className={`label ${className}`}>
-			{children}
+			<div
+				onClick={handleClick}
+				onKeyDown={handleKeyDown}
+				role="button"
+				tabIndex={-1}
+				className={`label__content ${contentClassName}`}
+			>
+				{children}
+			</div>
 		</label>
 	);
 }
