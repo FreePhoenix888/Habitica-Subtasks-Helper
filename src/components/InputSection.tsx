28e0ca2876522b802fe9a1d0411d077e75e173a3
FreePhoenix <freephoenix888@gmail.com>
2021-06-02 14:24:08 +0600

    Context is added

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,37 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { FormContext } from './Form';
 
 interface Props {
 	children: JSX.Element | JSX.Element[];
 	className?: string;
 }
 
+interface InputSectionContextType {
+	onFocusHandler: (event: React.FocusEvent<HTMLDivElement>) => void;
+	onBlurHandler: (event: React.FocusEvent<HTMLDivElement>) => void;
+}
+
+export const InputSectionContext = createContext<InputSectionContextType>({
+	onFocusHandler: () => {},
+	onBlurHandler: () => {},
+});
 export function InputSection(props: Props): JSX.Element {
 	const { children, className = '' } = props;
+
+	const { activeInputSectionClassName, changeActiveInputSectionClassName } =
+		useContext(FormContext);
 	const [isHovered, changeIsHovered] = useState<boolean>(false);
+	const [isFocused, changeIsFocused] = useState<boolean>(false);
+	const isActive =
+		activeInputSectionClassName &&
+		className.includes(activeInputSectionClassName);
+	const isAnyActive = Boolean(activeInputSectionClassName);
+
+	const InputSectionContextValue = {
+		onFocusHandler: handleFocus,
+		onBlurHandler: handleBlur,
+	};
 
 	function handleMouseOver(event: React.MouseEvent<HTMLDivElement>) {
+		changeActiveInputSectionClassName(className);
 		changeIsHovered(true);
 	}
 
 	function handleMouseLeave(event: React.MouseEvent<HTMLDivElement>) {
+		if (!isFocused) {
+			changeActiveInputSectionClassName('');
+		}
 		changeIsHovered(false);
 	}
 
-   useEffect(() => {
-      if(isHovered){
-         
-      }
-   }, [isHovered])
+	function handleFocus(event: React.FocusEvent<HTMLDivElement>) {
+		changeActiveInputSectionClassName(className);
+		changeIsFocused(true);
+	}
+
+	function handleBlur(event: React.FocusEvent<HTMLDivElement>) {
+		if (!isHovered) {
+			changeActiveInputSectionClassName('');
+		}
+		changeIsFocused(false);
+	}
 
 	return (
 		<div
 			onMouseOver={handleMouseOver}
 			onMouseLeave={handleMouseLeave}
+			onFocus={handleFocus}
 			className={`div input-section ${className} ${
-				isHovered ? `input-section--hovered ${className}--hovered` : ''
-			}`}
+				isActive ? `input-section--Active ${className}--active` : ''
+			}
+			 ${
+					isAnyActive && !isActive
+						? `input-section--not-active ${className}--not-active`
+						: ''
+				}
+			`}
 		>
-			{children}
+			<InputSectionContext.Provider value={InputSectionContextValue}>
+				{children}
+			</InputSectionContext.Provider>
 		</div>
 	);
 }
