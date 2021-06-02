92ecb0002f9b16d1e40461a35faec82b1f26374f
FreePhoenix <freephoenix888@gmail.com>
2021-06-02 15:47:57 +0600

    Add return type

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 interface Props {
-	children:
-		| string
-		| JSX.Element
-		| JSX.Element[]
-		| Element
-		| Element[]
+	children: string | JSX.Element | JSX.Element[] | Element | Element[];
 	className?: string;
 }
 
-export function Paragraph(props: Props) {
+export function Paragraph(props: Props): JSX.Element {
 	const { children, className = '' } = props;
 	return <p className={`paragraph ${className}`}>{children}</p>;
 }
