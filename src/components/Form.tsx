f12fb3258f886134b2c58f7b6e356b90ca1796eb
FreePhoenix <freephoenix888@gmail.com>
2021-06-02 14:23:38 +0600

    The component is ready

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
 interface Props {
 	action: string;
@@ -6,12 +6,33 @@ interface Props {
 	className?: string;
 }
 
+interface FormContextType {
+	activeInputSectionClassName: string;
+	changeActiveInputSectionClassName: React.Dispatch<
+		React.SetStateAction<string>
+	>;
+}
+
+export const FormContext = createContext<FormContextType>({
+	activeInputSectionClassName: '',
+	changeActiveInputSectionClassName: () => {},
+});
+
 export function Form(props: Props): JSX.Element {
 	const { action, children, className = '' } = props;
 
+	const [activeInputSectionClassName, changeActiveInputSectionClassName] =
+		useState<string>('');
+	const FormContextValue = {
+		activeInputSectionClassName,
+		changeActiveInputSectionClassName,
+	};
+
 	return (
 		<form action={action} className={`form ${className}`}>
-			{children}
+			<FormContext.Provider value={FormContextValue}>
+				{children}
+			</FormContext.Provider>
 		</form>
 	);
 }
