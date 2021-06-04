c18de0514f89df6270fe941f843241366146ae29
FreePhoenix <freephoenix888@gmail.com>
2021-06-04 14:52:13 +0600

    Create forwardedRef and return focus() method

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,18 +2,29 @@ import React, {
 	MouseEvent,
 	useImperativeHandle,
 	MutableRefObject,
+	forwardRef,
+	useRef,
+	ForwardRefRenderFunction,
 } from 'react';
 
 interface Props {
 	onClickHandler?: (event: MouseEvent<HTMLButtonElement>) => void;
 	onKeyDownHandler?: (event: React.KeyboardEvent<HTMLElement>) => void;
 	children?: JSX.Element | JSX.Element[];
-	forwardedRef?: MutableRefObject<HTMLButtonElement | null>;
+	forwardedRef?: MutableRefObject<any>;
 	className?: string;
 }
 
-export function Button(props: Props): JSX.Element {
+export function Button(props: Props) {
 	const { className = '', children, forwardedRef = null } = props;
+
+	const buttonRef = useRef<HTMLButtonElement>(null);
+
+	useImperativeHandle<HTMLButtonElement, any>(forwardedRef, () => ({
+		focus() {
+			buttonRef.current?.focus();
+		},
+	}));
 
 	function handleClick(event: MouseEvent<HTMLButtonElement>) {
 		const { onClickHandler } = props;
@@ -35,7 +46,7 @@ export function Button(props: Props): JSX.Element {
 			className={`button ${className}`}
 			onClick={handleClick}
 			onKeyDown={handleKeyDown}
-			ref={forwardedRef}
+			ref={buttonRef}
 		>
 			{children}
 		</button>
