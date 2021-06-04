246637c5dc0888b6fdeaa558d99df08a6d5656a3
FreePhoenix <freephoenix888@gmail.com>
2021-06-04 15:26:26 +0600

    Make Modal to work properly

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,7 @@ import React, {
 import { createPortal } from 'react-dom';
 import { Button } from './Button';
 import { Span } from './Span';
+import { MessageContainerContext } from './MessageContainer';
 
 interface Props {
 	isOpen: boolean;
@@ -30,13 +31,23 @@ let buttonRef: RefObject<HTMLButtonElement>;
 export function Modal(props: Props): React.ReactPortal | null {
 	const { children, className = '', isOpen } = props;
 
+	// Ref to the button to focus it
 	buttonRef = useRef<HTMLButtonElement>(null);
 
-	useEffect(() => {
-		buttonRef.current?.focus();
-	}, []);
+	useEffect(() => {}, []);
+
+	// Import setIsOpen to close modal
+	const { setIsOpen } = useContext(MessageContainerContext);
 
 	function handleClick(event: React.MouseEvent<HTMLElement>) {
+		// Default behavioir
+		const target = event.target as HTMLElement;
+
+		if (target.className.includes('modal-close')) {
+			setIsOpen(false);
+		}
+
+		// Custom behavior
 		const { onClickHandler } = props;
 		if (onClickHandler) {
 			onClickHandler(event);
@@ -44,6 +55,22 @@ export function Modal(props: Props): React.ReactPortal | null {
 	}
 
 	function handleKeyDown(event: React.KeyboardEvent<HTMLElement>) {
+		// Default behavior
+		const allowedKeys =
+			event.metaKey ||
+			event.ctrlKey ||
+			event.shiftKey ||
+			event.altKey ||
+			event.key === 'ArrowUp' ||
+			event.key === 'ArrowRight' ||
+			event.key === 'ArrowDown' ||
+			event.key === 'ArrowLeft';
+
+		if (!allowedKeys) {
+			setIsOpen(false);
+		}
+
+		// Custom behavior
 		const { onKeyDownHandler } = props;
 		if (onKeyDownHandler) {
 			onKeyDownHandler(event);
@@ -52,17 +79,20 @@ export function Modal(props: Props): React.ReactPortal | null {
 
 	return isOpen
 		? createPortal(
-				<div className={`modal ${className}`}>
-					<div className="modal__content">{children}</div>
+				<>
+					<div className={`modal ${className}`}>
+						<div className="modal__content">{children}</div>
+						<div className="modal__info-close">
+							<Span>Press any key or outside this window to close.</Span>
+						</div>
+					</div>
 					<Button
+					className="modal-close"
+						forwardedRef={buttonRef}
 						onClickHandler={handleClick}
 						onKeyDownHandler={handleKeyDown}
-						className="modal__close"
-						forwardedRef={buttonRef}
-					>
-						<Span>Press any key or outside this window to close.</Span>
-					</Button>
-				</div>,
+					/>
+				</>,
 				document.body
 		  )
 		: null;
