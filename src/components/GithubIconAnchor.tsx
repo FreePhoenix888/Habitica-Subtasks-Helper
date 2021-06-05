7db87fad26646eaac560bbfc30553a44ae681032
FreePhoenix <freephoenix888@gmail.com>
2021-06-05 15:43:15 +0600

    Add new component

---
+++ src/components/GithubIconAnchor.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GithubIcon } from './GithubIcon';
+import { Anchor } from './Anchor';
+
+interface Props {
+	href?: string;
+	anchorClassName?: string;
+	iconClassName?: string;
+}
+
+export function GithubIconAnchor(props: Props): JSX.Element {
+	const {
+		href = 'https://github.com/FreePhoenix888',
+		anchorClassName,
+		iconClassName,
+	} = props;
+
+	return (
+		<Anchor href={href} className={`anchor ${anchorClassName}`}>
+			<GithubIcon className={`${iconClassName}`} />
+		</Anchor>
+	);
+}
