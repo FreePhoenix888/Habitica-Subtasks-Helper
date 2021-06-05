12ea8bc2c69afbdfd78e4042563c830fc081a5bf
FreePhoenix <freephoenix888@gmail.com>
2021-06-05 20:46:52 +0600

    Add default value. Update filename

--- src/components/BlurIcon.tsx
+++ src/components/BlurIcon.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ReactComponent as BlurIconSVG } from '../media/images/lens_blur.svg';
+import { ReactComponent as BlurIconSVG } from '../media/images/lens_blur_icon.svg';
 
 interface Props {
 	className?: string;
 }
 
 export function BlurIcon(props: Props) {
-	const { className } = props;
+	const { className = '' } = props;
 
 	return <BlurIconSVG className={`svg icon blur-icon ${className}`} />;
 }
