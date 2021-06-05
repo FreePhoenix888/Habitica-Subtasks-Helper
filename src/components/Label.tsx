10a25aca98299dface87ba05df9c4298f99bc16c
FreePhoenix <freephoenix888@gmail.com>
2021-06-05 14:23:41 +0600

    Remove unused props

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -4,8 +4,6 @@ type Props = {
 	className?: string;
 	htmlFor: string;
 	children?: string | Element | Element[] | JSX.Element | JSX.Element[];
-	onClickHandler?: (event: React.MouseEvent<HTMLElement>) => void;
-	onKeyDownHandler?: (event: React.KeyboardEvent<HTMLElement>) => void;
 };
 
 export function Label(props: Props): JSX.Element {
