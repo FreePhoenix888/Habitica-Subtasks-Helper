9975ac55c7ff46510f75f7d73e54984da534f6e8
FreePhoenix <freephoenix888@gmail.com>
2021-06-06 21:37:25 +0600

    Add target prop

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+enum TargetValues {
+	'_self',
+	'_blank',
+	'_parent',
+	'_top',
+}
+
 interface Props {
 	children?: JSX.Element | JSX.Element[];
 	className?: string;
 	href: string;
+	target?: keyof typeof TargetValues;
 }
 
 export function Anchor(props: Props): JSX.Element {
-	const { href, children, className = '' } = props;
+	const { href, children, className = '', target = '_blank' } = props;
 	return (
-		<a className={`anchor ${className}`} href={href}>
+		<a className={`anchor ${className}`} href={href} target={target}>
 			{children}
 		</a>
 	);
