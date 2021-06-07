6231559994d111a005c7a2b0e45570b82955615b
FreePhoenix <freephoenix888@gmail.com>
2021-06-07 18:59:17 +0600

    Change almost all component

--- src/components/ModernRadioButtonGroup.tsx
+++ src/components/ModernRadioButtonGroup.tsx
@@ -1,45 +1,61 @@
-import React, { createContext, useState } from 'react';
+import React, {
+	ChangeEvent,
+	createContext,
+	Dispatch,
+	SetStateAction,
+	useState,
+} from 'react';
 
-type CheckedInputChangeEventType =
-	| React.ChangeEvent<HTMLInputElement>
-	| undefined;
-
-interface CheckedInputContextType {
-	handleChange: (value: CheckedInputChangeEventType) => void;
-	checkedInputChangeEvent: CheckedInputChangeEventType;
+interface CheckedRadioContextType {
+	checkedRadioValue: string;
+	setCheckedRadioValue:
+		| ((event: ChangeEvent<HTMLInputElement>) => void)
+		| undefined;
 }
 
-export const CheckedInputContext = createContext<
-	Partial<CheckedInputContextType>
->({});
+export const CheckedRadioContext = createContext<CheckedRadioContextType>({
+	checkedRadioValue: '',
+	setCheckedRadioValue: undefined,
+});
 
 interface Props {
-	name: string;
 	children: JSX.Element | JSX.Element[];
+	defaultChecked?: string;
 	groupClassName?: string;
+	name: string;
+	onChangeHandler?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const NameContext = createContext<string>('');
 export function ModernRadioButtonGroup(props: Props): JSX.Element {
-	const { children, groupClassName = '', name } = props;
-	const [checkedInputChangeEvent, changeCheckedInputChangeEvent] =
-		useState<CheckedInputChangeEventType>(undefined);
+	const { name, children, groupClassName = '', defaultChecked = '' } = props;
+	const [checkedRadioValue, setCheckedRadioValue] =
+		useState<string>(defaultChecked);
 
-	function handleChange(value: CheckedInputChangeEventType) {
-		changeCheckedInputChangeEvent(value);
+	function handleChange(event: ChangeEvent<HTMLInputElement>) {
+		const { target } = event;
+		const { value } = target;
+		// Default behavior
+		setCheckedRadioValue(value);
+
+		// Custom behavior
+		const { onChangeHandler } = props;
+		if (onChangeHandler) {
+			onChangeHandler(event);
+		}
 	}
 
-	const CheckedInputContextValue = {
-		checkedInputChangeEvent,
-		handleChange,
+	const CheckedInputContextValue: CheckedRadioContextType = {
+		checkedRadioValue,
+		setCheckedRadioValue: handleChange,
 	};
 
 	return (
 		<div className={`modern-radio-button-group ${groupClassName}`}>
 			<NameContext.Provider value={name}>
-				<CheckedInputContext.Provider value={CheckedInputContextValue}>
+				<CheckedRadioContext.Provider value={CheckedInputContextValue}>
 					{children}
-				</CheckedInputContext.Provider>
+				</CheckedRadioContext.Provider>
 			</NameContext.Provider>
 		</div>
 	);
