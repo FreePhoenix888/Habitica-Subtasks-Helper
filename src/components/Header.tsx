6127e99aec1a3ffc75b5bcf3ffc1828890c7c635
FreePhoenix <freephoenix888@gmail.com>
2021-06-07 16:04:09 +0600

    Add children prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { ChangeEvent, Context, createContext, useState } from 'react';
 import { ToggleSwitchBlur } from './ToggleSwitchBlur';
 import { IconGithubAnchor } from './IconGithubAnchor';
 import '../styles/components/header.scss';
 
 interface Props {
+	children?: JSX.Element | JSX.Element[];
 	className?: string;
 }
 
 export function Header(props: Props): JSX.Element {
-	const { className = '' } = props;
+	const { children, className = '' } = props;
 
 	return (
 		<div className={`header ${className}`}>
-			<ToggleSwitchBlur className="header__toggle-switch header__toggle-switch-blur" />
 			<IconGithubAnchor anchorClassName="header__anchor-github" />
+			{children}
 		</div>
 	);
 }
