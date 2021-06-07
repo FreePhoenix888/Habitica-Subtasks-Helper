4c452bbc9ef743f9cf11a7ebff1a647a5b723a7f
FreePhoenix <freephoenix888@gmail.com>
2021-06-07 17:46:20 +0600

    Add defaultValue and onChangeHandler props

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import '../styles/components/inputGeneral.scss';
 import '../styles/components/textarea.scss';
 
 interface Props {
 	className?: string;
+	defaultValue?: string;
 	id?: string;
 	name: string;
+	onChangeHandler?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
 	placeholder?: string;
 	wrap?: 'hard' | 'soft';
 }
 
 export function Textarea(props: Props): JSX.Element {
-	const [value, changeValue] = useState<string>('');
 	const {
 		className = '',
 		placeholder = '',
 		id = '',
 		name,
 		wrap = 'soft',
+		defaultValue = '',
 	} = props;
 
+	const [value, changeValue] = useState<string>(defaultValue);
+
 	function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+		const { onChangeHandler } = props;
+		if (onChangeHandler) {
+			onChangeHandler(event);
+		}
 		changeValue(event.target.value);
 	}
+
 	return (
 		<>
 			<textarea
