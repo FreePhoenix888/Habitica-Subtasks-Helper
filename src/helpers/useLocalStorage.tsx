0a7ba5947759acf601a8b7beed069e5fdb61d64c
FreePhoenix <freephoenix888@gmail.com>
2021-06-07 17:47:07 +0600

    Add new helper

---
+++ src/helpers/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+	const [storedValue, setStoredValue] = useState<T>(() => {
+		const item = window.localStorage.getItem(key);
+
+		return item ? (JSON.parse(item) as T) : initialValue;
+	});
+
+	function setValue(value: T | ((value: T) => T)) {
+		const valueToStore = value instanceof Function ? value(storedValue) : value;
+
+		setStoredValue(valueToStore);
+
+		window.localStorage.setItem(key, JSON.stringify(valueToStore));
+	}
+
+	return [storedValue, setValue] as const;
+}
