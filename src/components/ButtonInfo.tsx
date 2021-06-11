b8bd64a1df3b8683fc82083e6341d3b382dd146f
FreePhoenix <freephoenix888@gmail.com>
2021-06-11 14:12:52 +0600

    Pass event handlers from props directly

--- src/components/ButtonInfo.tsx
+++ src/components/ButtonInfo.tsx
@@ -8,48 +8,28 @@ interface Props {
 	onFocusHandler?: (event: React.FocusEvent<HTMLElement>) => void;
 	onKeyDownHandler?: (event: React.KeyboardEvent<HTMLElement>) => void;
 	onMouseOverHandler?: (event: React.MouseEvent<HTMLElement>) => void;
-};
+}
 
 export function ButtonInfo(props: Props): JSX.Element {
-	const { className = '', hidden = false } = props;
+	const {
+		className = '',
+		hidden = false,
+		onClickHandler,
+		onFocusHandler,
+		onKeyDownHandler,
+		onMouseOverHandler,
+	} = props;
 
-	function handleMouseOver(event: React.MouseEvent<HTMLElement>) {
-		const { onMouseOverHandler } = props;
-		if (onMouseOverHandler) {
-			onMouseOverHandler(event);
-		}
-	}
-
-	function handleClick(event: React.MouseEvent<HTMLElement>) {
-		const { onClickHandler } = props;
-		if (onClickHandler) {
-			onClickHandler(event);
-		}
-	}
-
-	function handleKeyDown(event: React.KeyboardEvent<HTMLElement>) {
-		const { onKeyDownHandler } = props;
-		if (onKeyDownHandler) {
-			onKeyDownHandler(event);
-		}
-	}
-
-	function handleFocus(event: React.FocusEvent<HTMLElement>) {
-		const { onFocusHandler } = props;
-		if (onFocusHandler) {
-			onFocusHandler(event);
-		}
-	}
 	return (
 		<>
 			<button
 				className={`button button-info ${className}`}
 				hidden={hidden}
 				type="button"
-				onClick={handleClick}
-				onFocus={handleFocus}
-				onKeyDown={handleKeyDown}
-				onMouseOver={handleMouseOver}
+				onClick={onClickHandler}
+				onFocus={onFocusHandler}
+				onKeyDown={onKeyDownHandler}
+				onMouseOver={onMouseOverHandler}
 			>
 				?
 			</button>
