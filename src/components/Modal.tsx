a1d73cf1d1363745a3f4d641bd46b556e5545ae3
FreePhoenix <freephoenix888@gmail.com>
2021-06-11 13:29:26 +0600

    Rename MessasgeContainerContext

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import React, { RefObject, useContext, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { Span } from './Span';
-import { MessageContainerContext } from './MessageContainer';
+import { Context as MessageContainerContext } from './MessageContainer';
 import '../styles/components/modal.scss';
 
 interface Props {
