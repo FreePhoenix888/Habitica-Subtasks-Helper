fbe296a31b32fac1695dbf71d569f4563d722264
FreePhoenix <freephoenix888@gmail.com>
2021-06-11 14:10:24 +0600

    Make separator modal workable

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import {
 	ModernRadioButtonGroup,
 	ModernRadioButtonGroupPreservingValue,
 	ModalButtonInfo,
-	MessageContainer,
 	Modal,
 	Header,
 	ToggleSwitch,
@@ -40,163 +39,167 @@ function App(): JSX.Element {
 		setIsToggleSwitchBlurChecked(checked);
 		setlLocalStorageToggleSwitchChecked(checked);
 	}
+
+	const [isSeparatorModalOpen, setIsSeparatorModalOpen] = useState(false);
 	return (
 		<>
-			<MessageContainer>
-				<Header>
-					<ToggleSwitchBlur
-						name={toggleSwitchBlurName}
-						onChangeHandler={toggleSwitchBlurOnChangeHandler}
-						isChecked={isToggleSwitchBlurChecked}
-						labelClassName="header__toggle-switch header__toggle-switch-blur"
-					/>
-				</Header>
-				<div className="container">
-					<h1>Habitica Subtasks Helper</h1>
-					<Form
-						action="#"
-						className="task-form"
-						isBlurOn={isToggleSwitchBlurChecked}
-					>
-						<InputSection className="task-title">
-							<Label className="task-title__label" htmlFor="taskTitle">
-								Title
-							</Label>
-							<InputPreservingValue
-								className="task-title__input"
-								id="taskTitle"
-								name="task_title"
-								placeholder="The Venus Project Conception."
-							/>
-						</InputSection>
+			<Header>
+				<ToggleSwitchBlur
+					name={toggleSwitchBlurName}
+					onChangeHandler={toggleSwitchBlurOnChangeHandler}
+					isChecked={isToggleSwitchBlurChecked}
+					labelClassName="header__toggle-switch header__toggle-switch-blur"
+				/>
+			</Header>
+			<div className="container">
+				<h1>Habitica Subtasks Helper</h1>
+				<Form
+					action="#"
+					className="task-form"
+					isBlurOn={isToggleSwitchBlurChecked}
+				>
+					<InputSection className="task-title">
+						<Label className="task-title__label" htmlFor="taskTitle">
+							Title
+						</Label>
+						<InputPreservingValue
+							className="task-title__input"
+							id="taskTitle"
+							name="task_title"
+							placeholder="The Venus Project Conception."
+						/>
+					</InputSection>
 
-						<InputSection className="task-subtasks">
-							<Label className="task-subtasks__label" htmlFor="taskSubtasks">
-								Subtasks
-							</Label>
-							<TextareaPreservingValue
-								className="textarea task-subtasks__textarea"
-								id="taskSubtasks"
-								name="task_subtasks"
-								placeholder={`What is the Venus Project?
+					<InputSection className="task-subtasks">
+						<Label className="task-subtasks__label" htmlFor="taskSubtasks">
+							Subtasks
+						</Label>
+						<TextareaPreservingValue
+							className="textarea task-subtasks__textarea"
+							id="taskSubtasks"
+							name="task_subtasks"
+							placeholder={`What is the Venus Project?
 								Aims, Proposals.
 								FAQ.
 								Free e-Books.
 								Recommended books.`}
-								wrap="soft"
-							/>
-						</InputSection>
+							wrap="soft"
+						/>
+					</InputSection>
 
-						<InputSection className="task-separator">
-							<div className="label-section label__section">
-								<Label
-									className="task-separator__label"
-									htmlFor="taskSeparator"
-								>
-									<Span>Separator</Span>
-								</Label>
-								<ModalButtonInfo className="task-separator__modal" />
-							</div>
-
-							<InputPreservingValue
-								className="input--little task-separator__input "
-								id="taskSeparator"
-								name="task_separator"
-								placeholder="\n"
-								autoSize
-							/>
-						</InputSection>
-
-						<InputSection className="task-tags">
-							<Label className="task-tags__label" htmlFor="taskTags">
-								Tags
+					<InputSection className="task-separator">
+						<div className="label-section label__section">
+							<Label className="task-separator__label" htmlFor="taskSeparator">
+								<Span>Separator</Span>
 							</Label>
-							<InputPreservingValue
-								className="task-tags__input "
-								id="taskTags"
-								name="task_tags"
-								placeholder="Self-Development, Psychology"
+							<ModalButtonInfo
+								className="task-separator__modal"
+								setIsOpen={setIsSeparatorModalOpen}
 							/>
-						</InputSection>
+						</div>
 
-						<InputSection className="task-difficulty">
-							<Label htmlFor="taskDifficulty1">Difficulty</Label>
-							<ModernRadioButtonGroupPreservingValue
-								name="task_difficulty"
-								groupClassName="task-difficulty__modern-radio-button-group"
-							>
-								{(function modernRadioButtons() {
-									// Difficulty Section Radio Buttons
-									const modernRadioButtons: JSX.Element[] = [];
+						<InputPreservingValue
+							className="input--little task-separator__input "
+							id="taskSeparator"
+							name="task_separator"
+							placeholder="\n"
+							autoSize
+						/>
+					</InputSection>
 
-									for (let i = 0; i < 4; i++) {
-										const stars: JSX.Element[] = [];
-										for (let j = 1; j < i + 2; j++) {
-											stars.push(
-												<StarSVG
-													className="svg star-SVG modern-radio-button__svg modern-radio-button__star-svg"
-													key={`${i}:${j}`}
-												/>
-											);
-										}
+					<InputSection className="task-tags">
+						<Label className="task-tags__label" htmlFor="taskTags">
+							Tags
+						</Label>
+						<InputPreservingValue
+							className="task-tags__input "
+							id="taskTags"
+							name="task_tags"
+							placeholder="Self-Development, Psychology"
+						/>
+					</InputSection>
 
-										modernRadioButtons.push(
-											<ModernRadioButton
-												htmlFor={`taskDifficulty${i + 1}`}
-												id={`taskDifficulty${i + 1}`}
-												inputClassName="task-difficulty-modern-radio-button__input"
-												key={i}
-												labelClassName="task-difficulty-modern-radio-button__label"
-												radioButtonClassName="radio-button-container--little task-difficulty__modern-radio-button-container"
-												value={`${i + 1}`}
-											>
-												{stars}
-											</ModernRadioButton>
+					<InputSection className="task-difficulty">
+						<Label htmlFor="taskDifficulty1">Difficulty</Label>
+						<ModernRadioButtonGroupPreservingValue
+							name="task_difficulty"
+							groupClassName="task-difficulty__modern-radio-button-group"
+						>
+							{(function modernRadioButtons() {
+								// Difficulty Section Radio Buttons
+								const modernRadioButtons: JSX.Element[] = [];
+
+								for (let i = 0; i < 4; i++) {
+									const stars: JSX.Element[] = [];
+									for (let j = 1; j < i + 2; j++) {
+										stars.push(
+											<StarSVG
+												className="svg star-SVG modern-radio-button__svg modern-radio-button__star-svg"
+												key={`${i}:${j}`}
+											/>
 										);
 									}
-									return modernRadioButtons;
-								})()}
-							</ModernRadioButtonGroupPreservingValue>
-						</InputSection>
-						<InputSection className="input-section task-notes">
-							<Label className="task-notes__label" htmlFor="taskNotes">
-								Notes
-							</Label>
-							<TextareaPreservingValue
-								className="textarea task-notes__textarea"
-								id="taskNotes"
-								name="task_notes"
-								placeholder="Read it without any prejudices as any new ideas."
-								wrap="soft"
-							/>
-						</InputSection>
-						<InputSection className="input-section task-amount">
-							<Label className="task-amount__label" htmlFor="taskSeparator">
-								<Span>Amount</Span>
-							</Label>
-							<InputPreservingValue
-								className="input--little task-amount__input "
-								id="taskAmount"
-								name="task_amount"
-								placeholder="\n"
-								autoSize
-							/>
-						</InputSection>
-					</Form>
-				</div>
-				<Modal className="task-separator__modal">
-					<Paragraph className="modal__paragraph">
-						{`You can use any symbols or regular expression to break your text into parts.
 
-			Example:
-			Brush teeth, Take a bath, Training, Learning with separator , will lead to this result:
-			🞄Brush teeth
-			🞄Take a bath
-			🞄Training
-			🞄Learning`}
-					</Paragraph>
-				</Modal>
-			</MessageContainer>
+									modernRadioButtons.push(
+										<ModernRadioButton
+											htmlFor={`taskDifficulty${i + 1}`}
+											id={`taskDifficulty${i + 1}`}
+											inputClassName="task-difficulty-modern-radio-button__input"
+											key={i}
+											labelClassName="task-difficulty-modern-radio-button__label"
+											radioButtonClassName="radio-button-container--little task-difficulty__modern-radio-button-container"
+											value={`${i + 1}`}
+										>
+											{stars}
+										</ModernRadioButton>
+									);
+								}
+								return modernRadioButtons;
+							})()}
+						</ModernRadioButtonGroupPreservingValue>
+					</InputSection>
+					<InputSection className="input-section task-notes">
+						<Label className="task-notes__label" htmlFor="taskNotes">
+							Notes
+						</Label>
+						<TextareaPreservingValue
+							className="textarea task-notes__textarea"
+							id="taskNotes"
+							name="task_notes"
+							placeholder="Read it without any prejudices as any new ideas."
+							wrap="soft"
+						/>
+					</InputSection>
+					<InputSection className="input-section task-amount">
+						<Label className="task-amount__label" htmlFor="taskSeparator">
+							<Span>Amount</Span>
+						</Label>
+						<InputPreservingValue
+							className="input--little task-amount__input "
+							id="taskAmount"
+							name="task_amount"
+							placeholder="\n"
+							autoSize
+						/>
+					</InputSection>
+				</Form>
+			</div>
+			<Modal
+				className="task-separator__modal"
+				isOpen={isSeparatorModalOpen}
+				setIsOpen={setIsSeparatorModalOpen}
+			>
+				<Paragraph className="modal__paragraph">
+					{`You can use any symbols or regular expression to break your text into parts.
+
+					Example:
+					Brush teeth, Take a bath, Training, Learning with separator , will lead to this result:
+					🞄Brush teeth
+					🞄Take a bath
+					🞄Training
+					🞄Learning`}
+				</Paragraph>
+			</Modal>
 		</>
 	);
 }
