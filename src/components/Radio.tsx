b2ad7921b19a16e05b94900c976fa8b5ba32286f
FreePhoenix <freephoenix888@gmail.com>
2021-06-17 22:35:24 +0600

    Remove unused trash

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,53 +1,23 @@
-import React, { createRef, useEffect } from 'react';
-import '../styles/components/inputGeneral.scss';
+import React from 'react';
 
 interface Props {
 	className?: string;
-	hidden?: boolean;
 	id?: string;
-	isChecked: boolean;
+	isChecked?: boolean;
 	name: string;
-	onBlurHandler?: (value: React.FocusEvent<HTMLInputElement>) => void;
-	onChangeHandler?: (value: React.ChangeEvent<HTMLInputElement>) => void;
-	onFocusHandler?: (value: React.FocusEvent<HTMLInputElement>) => void;
-	tabIndex?: number;
 	value: string;
 }
-
 export function Radio(props: Props): JSX.Element {
-	const {
-		value,
-		name,
-		className = '',
-		id = '',
-		tabIndex = 0,
-		hidden = false,
-		isChecked,
-		onChangeHandler,
-		onFocusHandler,
-		onBlurHandler,
-	} = props;
+	const { name, id, className = '', value, isChecked } = props;
 
-	const radioRef = createRef<HTMLInputElement>();
-
-	useEffect(() => {
-		radioRef.current.checked = true;
-	}, [isChecked]);
 	return (
-		<>
-			<input
-				className={`radio-button ${className}`}
-				hidden={hidden}
-				id={id}
-				name={name}
-				ref={radioRef}
-				tabIndex={tabIndex}
-				type="radio"
-				value={value}
-				onBlur={onBlurHandler}
-				onChange={onChangeHandler}
-				onFocus={onFocusHandler}
-			/>
-		</>
+		<input
+			checked={isChecked}
+			className={`radio ${className}`}
+			id={id}
+			name={name}
+			type="radio"
+			value={value}
+		/>
 	);
 }
