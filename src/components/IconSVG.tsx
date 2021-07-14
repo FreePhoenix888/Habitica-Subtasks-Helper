21077d0009e248982dbf9269859f6a8f23385144
FreePhoenix <freephoenix888@gmail.com>
2021-07-14 16:01:45 +0600

    Update props and className

--- src/components/IconSVG.tsx
+++ src/components/IconSVG.tsx
@@ -2,12 +2,12 @@ import React, { Component, FC } from 'react';
 import '../styles/components/icon.scss';
 
 interface Props {
-	Icon?: FC<{ className: string }>;
+	Icon?: FC<React.SVGProps<SVGSVGElement>>;
 	className?: string;
 }
 
 export function IconSVG(props: Props) {
 	const { className = '', Icon } = props;
 
-	return <Icon className={`svg icon ${className}`} />;
+	return <Icon className={`icon svg ${className}`} />;
 }
