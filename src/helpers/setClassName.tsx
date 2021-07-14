23df8673bfab266b58719e3ad58ec4a7bf2b6db4
FreePhoenix <freephoenix888@gmail.com>
2021-07-14 16:03:39 +0600

    Remove redundant code

--- src/helpers/setClassName.tsx
+++ src/helpers/setClassName.tsx
@@ -2,36 +2,20 @@ interface ModifiersType {
 	[key: string]: boolean;
 }
 
-export function setClassName(
-	defaultClassName = '',
-	customClassName = '',
-	modifiers?: ModifiersType
-) {
-	// Get every class name
-	const defaultClassNames = defaultClassName.split(' ');
-	const customClassNames = customClassName.split(' ');
-	// Concatenate default class names with custom class names
-	let outputClassName = `${defaultClassName} ${customClassName} `;
-
+export function setClassName(className = '', modifiers?: ModifiersType) {
+	let outputClassName = `${className} `;
 	if (modifiers) {
+		const classNames = className.split(' ');
 		Object.entries(modifiers).forEach(([modifier, condition]) => {
 			// Check if class name need modifier
 			if (condition) {
 				// Add modifier to every default class name
-				const modifiedDefaultClassNames = defaultClassNames.map(
+				const modifiedClassNames = classNames.map(
 					(className) => `${className}--${modifier} `
 				);
-				// Add modifier to every custom class name
-				const modifiedCustomClassNames = customClassNames.map(
-					(className) => `${className}--${modifier} `
-				);
-				// Concatenate modified default class names with modified custom class names
-				outputClassName += `${modifiedDefaultClassNames.join(
-					' '
-				)} ${modifiedCustomClassNames.join(' ')} `;
+				outputClassName += modifiedClassNames.join(' ');
 			}
 		});
 	}
-
 	return outputClassName;
 }
