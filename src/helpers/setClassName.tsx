d993c444b9211cafa638f4e2c7ce0992ca974298
FreePhoenix <freephoenix888@gmail.com>
2021-07-16 03:45:20 +0600

    Remove redundant code. Make lightweight

--- src/helpers/setClassName.tsx
+++ src/helpers/setClassName.tsx
@@ -1,21 +1,16 @@
-interface ModifiersType {
-	[key: string]: boolean;
-}
-
-export function setClassName(className = '', modifiers?: ModifiersType) {
-	let outputClassName = `${className} `;
-	if (modifiers) {
-		const classNames = className.split(' ');
-		Object.entries(modifiers).forEach(([modifier, condition]) => {
-			// Check if class name need modifier
-			if (condition) {
-				// Add modifier to every default class name
-				const modifiedClassNames = classNames.map(
-					(className) => `${className}--${modifier} `
-				);
-				outputClassName += modifiedClassNames.join(' ');
-			}
-		});
-	}
-	return outputClassName;
+export function setClassName(
+	className = '',
+	modifiers: Record<string, boolean>
+) {
+	const classNames = className.split(' ');
+	const outputClassNames = [...classNames];
+	Object.entries(modifiers).forEach(([modifier, condition]) => {
+		if (condition) {
+			const modifiedClassName = classNames.map(
+				(className) => `${className}--${modifier}`
+			);
+			outputClassNames.push(...modifiedClassName);
+		}
+	});
+	return outputClassNames.join(' ');
 }
