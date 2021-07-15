cb977a0a68d0a85c17569dc13d18e3767f80003d
FreePhoenix <freephoenix888@gmail.com>
2021-07-16 03:44:00 +0600

    Add icons

---
+++ src/iconComponents.tsx
@@ -0,0 +1,6 @@
+export { IconBlur } from './components/icons/IconBlur';
+export { IconGithub } from './components/icons/IconGithub';
+export { IconCheck } from './components/icons/IconCheck';
+export { IconInfo } from './components/icons/IconInfo';
+export { IconWarning } from './components/icons/IconWarning';
+export { IconError } from './components/icons/IconError';
