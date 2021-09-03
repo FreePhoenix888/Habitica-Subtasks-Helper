e58fb78f52dee8ae8cd9b169fc745d567b9eb901
FreePhoenix <freephoenix888@gmail.com>
2021-09-03 19:42:37 +0600

    Update setClassName

--- src/helpers/setClassName.tsx
+++ src/helpers/setClassName.tsx
@@ -1,16 +1,18 @@
 export function setClassName(
 	className = '',
-	modifiers: Record<string, boolean>
-) {
-	const classNames = className.split(' ');
+	modifiers?: Record<string, boolean>
+):string {
+	const classNames = className.trim().split(' ');
 	const outputClassNames = [...classNames];
-	Object.entries(modifiers).forEach(([modifier, condition]) => {
-		if (condition) {
-			const modifiedClassName = classNames.map(
-				(className) => `${className}--${modifier}`
-			);
-			outputClassNames.push(...modifiedClassName);
-		}
-	});
+	if(modifiers){
+		Object.entries(modifiers).forEach(([modifier, condition]) => {
+			if (condition) {
+				const modifiedClassName = classNames.map(
+					(className) => `${className}--${modifier}`
+				);
+				outputClassNames.push(...modifiedClassName);
+			}
+		});
+	}
 	return outputClassNames.join(' ');
 }
