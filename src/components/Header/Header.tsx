85abf69f101934ab50d289e20d9d00d073ee07c9
FreePhoenix <freephoenix888@gmail.com>
2021-09-03 19:55:07 +0600

    Move Header to separate folder, improve code

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React, {FC, HTMLProps} from 'react';
+import {ToastContainer} from "react-toastify";
+import {Anchor} from "../Anchor/Anchor";
+import {ReactComponent as GithubIcon} from '../../media/images/icons/github.svg'
+import './header.scss';
+
+
+
+type IProps = HTMLProps<HTMLDivElement>;
+
+export const Header: FC = ({children, className = '', ...rest}: IProps) =>
+    <>
+        <div className={`header ${className}`} {...rest}>
+            <Anchor href="https://github.com/FreePhoenix888/Habitica-Subtasks-Helper" rel="noopener noreferrer"
+                    target="_blank"><GithubIcon/></Anchor>
+            {children}
+        </div>
+        <ToastContainer/>
+    </>;
