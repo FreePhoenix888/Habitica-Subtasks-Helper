b81f3898943520976cb5f9795214b9ab8e7a0775
FreePhoenix <freephoenix888@gmail.com>
2021-09-03 19:39:34 +0600

    Move Form to separate folder, make it a decorator that adds app className

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,6 @@
+import React, { HTMLProps } from 'react';
+import './form.scss';
+
+type IProps = HTMLProps<HTMLFormElement>;
+
+export const Form = ({ className, ...rest }: IProps):JSX.Element => <form className={`habitica-subtasks-helper-form ${className}`} {...rest} />
