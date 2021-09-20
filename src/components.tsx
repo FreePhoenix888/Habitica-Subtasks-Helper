02c679ce6766899b70e4b08aa1593985a271030d
FreePhoenix <freephoenix888@gmail.com>
2021-09-20 17:52:32 +0600

    Update exported components

--- src/components.tsx
+++ src/components.tsx
@@ -1,30 +1,22 @@
-export { Span } from './components/Span';
-export { Paragraph } from './components/Paragraph';
-export { Anchor } from './components/Anchor';
-export { Form } from './components/Form';
-export { TaskForm } from './components/TaskForm';
-export { InputSection } from './components/InputSection';
-export { Label } from './components/Label';
-export { Input } from './components/Input';
-export { InputPreservingValue } from './components/InputPreservingValue';
-export { Textarea } from './components/Textarea';
-export { TextareaPreservingValue } from './components/TextareaPreservingValue';
-export { Button } from './components/Button';
-export { ButtonSubmit } from './components/ButtonSubmit';
-export { Radio } from './components/Radio';
-export { ModernRadio } from './components/ModernRadio';
-export {
-	ModernRadioGroup,
-	ModernRadioGroupNameContext,
-	CheckedRadioValueContext,
-} from './components/ModernRadioGroup';
-export { ModernRadioGroupPreservingValue } from './components/ModernRadioGroupPreservingValue';
-export { ButtonInfo } from './components/ButtonInfo';
-export { ModalButtonInfo } from './components/ModalButtonInfo';
-export { Modal } from './components/Modal';
-export { Header } from './components/Header';
-export { ToggleSwitch } from './components/ToggleSwitch';
-export { ToggleSwitchBlur } from './components/ToggleSwitchBlur';
-export { ToastContainer, toast } from 'react-toastify';
+export {Anchor} from './components/Anchor/Anchor';
+export {Label} from './components/Label/Label';
+export {Paragraph} from './components/Paragraph/Paragraph';
+export {Code} from './components/Code/Code';
+export {Form} from './components/Form/Form';
+export {InputErrorMessage} from './components/Form/InputErrorMessage';
+export {Form as FinalForm, Field} from 'react-final-form';
+export {Input} from './components/Input/Input';
+export {TextArea} from './components/TextArea/TextArea';
+export {Radio} from './components/Radio/Radio';
+export {RadioGroup} from './components/Radio/RadioGroup';
+export {ButtonInfo} from './components/ButtonInfo';
+export {ModalButtonInfo} from './components/ModalButtonInfo';
+export {ModalsStorage, modalsStorageApi} from './components/Modal/ModalsStorage';
+export {Header} from './components/Header/Header';
+export {ToastContainer, toast} from 'react-toastify';
+export {Button} from './components/Button/Button';
+export {InfoButton} from './components/Button/InfoButton';
+export {SubmitButton} from './components/Button/SubmitButton';
+export {useForm, useWatch, useController, Controller} from 'react-hook-form';
 
 
