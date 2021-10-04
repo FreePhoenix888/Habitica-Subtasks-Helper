be25987d7bc3aba2b7359172fe0a4a6e211ef554
FreePhoenix <freephoenix888@gmail.com>
2021-10-04 12:14:15 +0600

    setClassName - Parse modifiers from className

--- src/helpers/setClassName.tsx
+++ src/helpers/setClassName.tsx
@@ -1,11 +1,16 @@
 export function setClassName(
     className = '',
-    modifiers?: Record<string, boolean>
+    modifiers: Record<string, boolean> = {}
 ): string {
     const classNames = className.trim().split(' ');
+    const allModifiers = modifiers;
+    classNames.forEach(className => {
+        const modifier = className.split('--')[1];
+        if(modifier) allModifiers[modifier] = true;
+    })
     const outputClassNames = [...classNames];
-    if (modifiers) {
-        Object.entries(modifiers).forEach(([modifier, condition]) => {
+    if (allModifiers) {
+        Object.entries(allModifiers).forEach(([modifier, condition]) => {
             if (condition) {
                 const modifiedClassName = classNames.map(
                     (className) => `${className}--${modifier}`
